f25226aeead862c850230e331365173072292d80
@Decoder <45496136+Deepulak@users.noreply.github.com>
2023-07-27 11:40:22 +0600

    first commit

---
+++ src/mock/profile.js
@@ -0,0 +1,23 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://www.facebook.com/its.elliott.decoder',       
+    icon: 'akar-icons:facebook-fill',        
+  },
+  {
+    link: 'https://www.instagram.com/its__elliott__/',
+    icon: 'akar-icons:instagram-fill',
+  },
+  {
+    link: 'https://www.linkedin.com/in/dee-pu-b84b73175/',
+    icon: 'akar-icons:linkedin-fill',
+  },
+];
+
+export const HERO_TITLES = ['Depu Pulak', 'Full-stack Developer', 'Javascript Enthusiast'];
+
+export const aboutParagraph =
+  'Highly motivated and enthusiastic Full Stack Developer with experience in designing, developing and maintaining web applications using technologies such as JavaScript, React, Node.js. ';
+
+export const contactEmail = 'dpulaak@gmail.com';
+
+export const documentTitle = 'Hello I am Depu Pulak | Full Stack Developer | Javascript Enthusiast'
